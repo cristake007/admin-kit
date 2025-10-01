4e522000912a4e2c0a55ad9611bf0707f481b1d3
cristake007 <popadorincristian@gmail.com>
2025-10-01 23:55:39 +0300

    Update install.sh

--- admin_kit/install.sh
+++ admin_kit/install.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 set -Euo pipefail
 
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-# Bootstrap + functions
-# shellcheck disable=SC1091
-source "$SCRIPT_DIR/scripts/bootstrap.sh"
-require "functions/functions.sh"
-
-echo_info "Creating project-root file"
+echo "Creating project-root file"
 touch .project-root
-echo_info "Starting AdminKit"
+echo "Starting AdminKit"
 bash admin_menu.sh
