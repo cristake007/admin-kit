e0bb64819a763d3e0c22465369ef650aa918aeb7
cristake007 <popadorincristian@gmail.com>
2026-03-08 23:34:10 +0200

    Delete scripts directory

--- scripts/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-BOOTSTRAP_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_ROOT="$(cd -- "$BOOTSTRAP_DIR/.." && pwd)"
-export PROJECT_ROOT
-
-# shellcheck disable=SC1091
-source "$PROJECT_ROOT/lib/core.sh"
